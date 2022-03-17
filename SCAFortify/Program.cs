2d76fe53ae57aa35838ed0848681c0a513e72a79
marishvinoj <43641320+marishvinoj@users.noreply.github.com>
2022-03-17 22:12:54 +0530

    Commit
    Commit

--- SCAFortify/Program.cs
+++ SCAFortify/Program.cs
@@ -18,7 +18,7 @@ namespace SCAFortify
         
         static void Info(object message, Exception ex, params object[] args)
         {
-            string InstanceName = GetName("Report Instance Name [{0}] - ", "textfile.txt");
+            string InstanceName = GetName("Report Instance Name [{0}] - ", "/textfile");
             if (message is string && args.Length > 0)
             {
                 string msg = string.Format(InstanceName + (string)message, args);
@@ -27,7 +27,7 @@ namespace SCAFortify
 
         static string GetName(string formatString, string fileName)
         {
-            File.Open("textfile.txt", FileMode.Open);
+            Directory.CreateDirectory(fileName);
             return string.Format(formatString, "");
         }
     }
