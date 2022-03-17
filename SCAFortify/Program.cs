2f22eecd0a0e3f295204b3fc8f593368c549d430
marishvinoj <43641320+marishvinoj@users.noreply.github.com>
2022-03-17 14:02:10 +0530

    commit

--- SCAFortify/Program.cs
+++ SCAFortify/Program.cs
@@ -12,5 +12,15 @@ namespace SCAFortify
         {
             var s = string.Format(args[0], "test");
         }
+        
+        
+        static void Info(object message, Exception ex, params object[] args)
+        {
+            string InstanceName = "Report Instance Name [{0}] - ";
+            if (message is string && args.Length > 0)
+            {
+                string msg = string.Format(InstanceName + (string)message, args);
+            }
+        }
     }
 }
