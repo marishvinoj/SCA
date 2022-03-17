8e17aaa105cc4126d6c44cb0812d42760683e3df
marishvinoj <43641320+marishvinoj@users.noreply.github.com>
2022-03-17 22:08:33 +0530

    Commit
    Commit

--- SCAFortify/Program.cs
+++ SCAFortify/Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,15 +18,16 @@ namespace SCAFortify
         
         static void Info(object message, Exception ex, params object[] args)
         {
-            string InstanceName = GetName("Report Instance Name [{0}] - ");
+            string InstanceName = GetName("Report Instance Name [{0}] - ", "textfile.txt");
             if (message is string && args.Length > 0)
             {
                 string msg = string.Format(InstanceName + (string)message, args);
             }
         }
 
-        static string GetName(string formatString)
+        static string GetName(string formatString, string fileName)
         {
+            File.Open("textfile.txt", FileMode.Open);
             return string.Format(formatString, "");
         }
     }
