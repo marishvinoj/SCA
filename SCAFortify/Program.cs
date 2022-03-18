96d4180d0a254c886ceb90383d268f56fa230e5f
marishvinoj <43641320+marishvinoj@users.noreply.github.com>
2022-03-18 14:37:25 +0530

    Commit
    Commit

--- SCAFortify/Program.cs
+++ SCAFortify/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,11 @@ namespace SCAFortify
         static void Main(string[] args)
         {
             object message = new ArgumentNullException();
+            //Data Source = (local); Initial Catalog = master; Integrated Security = True
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString =
+              "(local); Initial Catalog = master; Integrated Security = True";
+            conn.Open();
             Info(message, new Exception(), args);
         }
         
