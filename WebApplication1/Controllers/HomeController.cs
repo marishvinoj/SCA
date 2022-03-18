7091b3f104f26eba90b1bc1902c61c725176c386
marishvinoj <43641320+marishvinoj@users.noreply.github.com>
2022-03-18 18:12:01 +0530

    Commit
    Commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var path = "test";
+            test(path);
+            return View();
+        }
+
+        public void test(string path)
+        {
+            if (Directory.Exists(path))
+            {
+
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
